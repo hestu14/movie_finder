d468a7c604de3a40bb4b24e7c29563492c215ec0
hestu14 <hestu.14@gmail.com>
2020-08-12 22:11:25 +0700

    Updates

---
+++ precache-manifest.192c9a3c1023d9ebdd97f5a99785f7b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "762cdf660a3e58c9c103bd312debb806",
+    "url": "/movie_finder/index.html"
+  },
+  {
+    "revision": "1007348768e166826084",
+    "url": "/movie_finder/static/css/main.6929b00b.chunk.css"
+  },
+  {
+    "revision": "63b93dd4c8c00f999b55",
+    "url": "/movie_finder/static/js/2.2cac64e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_finder/static/js/2.2cac64e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1007348768e166826084",
+    "url": "/movie_finder/static/js/main.3ebd823e.chunk.js"
+  },
+  {
+    "revision": "0b97513c152d09ae6b2c",
+    "url": "/movie_finder/static/js/runtime-main.64271d1b.js"
+  }
+]);
